60535336b01e7521546801f74ff033d946fceb40
Aryan <aryan.motiondash@gmail.com>
2019-07-16 23:42:47 -0400

    Changed flex direction. Fixed Media Querys

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const navSlide = function() {
+//Hamburger menu
+
+const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
-  
-  burger.addEventListener("click", function() {
+
+  burger.addEventListener("click", function () {
     nav.classList.toggle("nav-active");
   });
 }
+
+function winSize() {
+  console.log(`
+Inner Width: ${this.innerWidth}
+Inner Height: ${this.innerHeight}
+Outer Width: ${this.outerWidth}
+Outer Height: ${this.outerHeight}
+`);
+}
+
+winSize();
 
 navSlide();
